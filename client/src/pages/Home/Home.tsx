bb91c9c2b2b3e3253183fbeb1f8cdec01e16f8f9
Bamba_DIOP <khadinho@live.fr>
2025-07-22 23:10:16 +0200

    work in progress

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -8,7 +8,6 @@ function Home() {
       </h1>
       <img src="/cab.svg" alt="Cheikh Ahmadou Bamba" className="cheikh-image" />
       <p className="dates">1853 - 1927</p>
-      <footer className="footer-home">© 2025 Cheikh Ahmadou Bamba</footer>
     </main>
   );
 }
