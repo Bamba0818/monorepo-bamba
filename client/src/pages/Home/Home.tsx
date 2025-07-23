a77de1b75b5dc9d66629d55c089ea3371cf41b42
Bamba_DIOP <khadinho@live.fr>
2025-07-23 17:58:31 +0200

    work in progress

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -6,8 +6,32 @@ function Home() {
       <h1 className="page-title">
         Découvrez les œuvres de Cheikh Ahmadou Bamba
       </h1>
-      <img src="/cab.svg" alt="Cheikh Ahmadou Bamba" className="cheikh-image" />
-      <p className="dates">1853 - 1927</p>
+
+      <div className="home-content">
+        <img
+          src="/cab.svg"
+          alt="Cheikh Ahmadou Bamba"
+          className="cheikh-image"
+        />
+
+        <section className="presentation">
+          <h2>Qui est Cheikh Ahmadou Bamba ?</h2>
+          <p>
+            Cheikh Ahmadou Bamba Mbacké était un érudit musulman et fondateur de la
+            confrérie soufie des Mourides. Né en 1853, il a consacré sa vie à la
+            spiritualité, à l’enseignement et à la non-violence face à la
+            colonisation.
+          </p>
+          <p>
+            Il est l’auteur de nombreux poèmes mystiques (les <strong>Khassidas</strong>)
+            écrits en arabe classique, célébrant Dieu et le Prophète Muhammad. Sa ville
+            sainte, <strong>Touba</strong>, est aujourd’hui un haut lieu du savoir et
+            de la foi musulmane.
+          </p>
+        </section>
+      </div>
+
+     
     </main>
   );
 }
